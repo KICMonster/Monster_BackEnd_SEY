448291d218e70072d54ab02ccda7cbf259ff5260
eyy <eyy950@gmail.com>
2024-05-26 23:14:42 +0900

    백엔드 엔티티 수정버전

--- src/main/java/com/monster/luvCocktail/domain/Enumeration/Role.java
+++
@@ -1,6 +0,0 @@
-package com.monster.luvCocktail.domain.Enumeration;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/java/com/monster/luvCocktail/domain/repository/ViewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monster.luvCocktail.domain.repository;
-
-import com.monster.luvCocktail.domain.entity.View;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ViewRepository extends JpaRepository<View, Long> {
-}
